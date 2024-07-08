1ae3d702936ec160ca87e07eea41f1a195a06939
Hritik <pypi.pioneer@gmail.com>
2024-07-08 16:58:04 +0530

    token age updated

--- src/v1/utils/secure/constants.ts
+++ src/v1/utils/secure/constants.ts
@@ -1,6 +1,6 @@
 // tokens and cookies age, and their expiry dates
 const COOKIE_AGE = 15 * 24 * 60 * 60 * 1000;  // 15 days in milliseconds
-const ACCESS_TOKEN_AGE = 15 * 24 * 60 * 60;  // 15 days in seconds
+const ACCESS_TOKEN_AGE = 30 * 24 * 60 * 60;  // 30 days in seconds
 const REFRESH_TOKEN_AGE = 30 * 24 * 60 * 60;  // 30 days in seconds
 
 export { COOKIE_AGE, ACCESS_TOKEN_AGE, REFRESH_TOKEN_AGE }
